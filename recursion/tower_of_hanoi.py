cfde286d064070fb34d118e5b8c698a891788b3b
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-22 21:44:06 +0530

    Tower of Hanoi problem in python

---
+++ recursion/tower_of_hanoi.py
@@ -0,0 +1,10 @@
+def THO(n, A, B, C):
+    if n == 1:
+        print("Move 1 from", A, "to", C)
+    else:
+        THO(n - 1, A, C, B)
+        print("Move", n, "from", A, "to", C)
+        THO(n - 1, B,A, C)
+
+
+THO(3, 'A', 'B','C' )
