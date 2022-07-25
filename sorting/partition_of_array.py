bbeddd13f98b457615795bb8304b787ce6313bc0
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-25 21:48:15 +0530

    Partition A Given Array

---
+++ sorting/partition_of_array.py
@@ -0,0 +1,23 @@
+def partition(arr, p):
+    n = len(arr)
+    arr[p], arr[n - 1] = arr[n - 1], arr[p]
+
+    temp = []
+
+    for x in arr:
+        if x <= arr[n - 1]:
+            temp.append(x)
+
+    for x in arr:
+        if x > arr[n - 1]:
+            temp.append(x)
+
+    for i in range(len(arr)):
+        arr[i] = temp[i]
+
+
+arr = [5, 13, 6, 9, 12, 8, 11]
+
+partition(arr, 5)
+
+print(*arr)
