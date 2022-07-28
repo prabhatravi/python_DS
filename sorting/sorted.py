3e3af0cd8f3f63f5b189791a4351e801f8752a7a
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-28 21:19:43 +0530

    sorted method in python

---
+++ sorting/sorted.py
@@ -0,0 +1,14 @@
+t = (10,12,5,1)
+print(sorted(t))
+
+s = {'gfg','courses','python'}
+print(sorted(s))
+
+st = 'gfg'
+print(sorted(st))
+
+d = {10:'gfg',15:'ide',5:'courses'}
+print(sorted(d))
+
+l = [(10,15),(1,8),(2,3)]
+print(sorted(l))
