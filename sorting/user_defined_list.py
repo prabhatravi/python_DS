5b85239b8b9e29614a476ea694f217ac8a6fd140
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-28 21:15:20 +0530

    some more testing with __it__ method

--- sorting/user_defined_list.py
+++ sorting/user_defined_list.py
@@ -2,12 +2,23 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    
+    '''def __lt__(self, other):
+        return self.x < other.x'''
+
+    def __lt__(self, other):
+    
+        if self.x == other.x:
+            return self.y < other.y
+        else:
+            return self.x < other.x
 
 def myFun(p):
     return p.x
 
-l = [Point(3, 5), Point(10, 5), Point(3, 8)]
-l.sort(key=myFun)
+l = [Point(3, 5), Point(10, 9), Point(1, 8)]
+#l.sort(key=myFun)
+l.sort()
 
 for i in l:
     print(i.x, i.y)
