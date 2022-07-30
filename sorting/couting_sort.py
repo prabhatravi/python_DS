12af97d5baa5b1bec3ee1813e91820f1f646e22c
Prabhat Kumar Ravi <prabhat.ravi.engg@gmail.com>
2022-07-30 22:54:34 +0530

    added generic counting sort

--- sorting/couting_sort.py
+++ sorting/couting_sort.py
@@ -8,7 +8,24 @@ def countingSort(arr, k):
             arr[index] = i
             index += 1
 
+def genericCountingSort(arr, k):
+    output = [0] * len(arr)
+    count = [0] * (k)
+    for x in arr:
+        count[x] += 1
+
+    for i in range(1, k):
+        count[i] += count[i-1]
+    for x in reversed(arr):
+        output[count[x]-1] = x
+        count[x] -= 1
+    for i in range(len(arr)):
+        arr[i] = output[i]
+
 arr = [1, 4, 4, 1, 0, 1]
 # o/p [0, 1, 1, 1, 4, 4]
 countingSort(arr, 6)
 print(arr)
+arr1 = [1, 4, 4, 2, 0, 1]
+genericCountingSort(arr1, 6)
+print(arr1)
